9f20d8b1b9d4aa6accee903a802e8909adc61c90
leandroherrera-poc <leandro.herrera@globant.com>
2025-10-22 16:40:32 -0500

    POC Pull Request (#1)
    * Add main.py to poc_1 branch
    
    * Add test.py to poc_1 branch

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main import hello
+
+
+class TestMain(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
